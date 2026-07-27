e76d786c63c83065f4b2e90391a5df034a07be6e
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-07-27 23:26:50 +0530

    Create dsa_03.java
    This is an anagram question using dsa

---
+++ dsa_03.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+
+        if (isAnagram(s, t)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
